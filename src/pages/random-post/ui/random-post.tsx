86ad870206166f8bd51d4efb93199d83c32a6224
HAROSHII <igorgrime@gmail.com>
2025-03-10 21:52:42 +0300

    feat: add Loader

--- src/pages/random-post/ui/random-post.tsx
+++ src/pages/random-post/ui/random-post.tsx
@@ -1,15 +1,20 @@
+import { Loader } from '@shared/ui/loader';
 import { useGetRandomPost } from '../api/use-get-random-post';
 
 export const RandomPostPage = () => {
   const { fetchRandomPost, error, loading, post } = useGetRandomPost();
+
   return (
     <>
       <h1>Рандомный пост</h1>
-      {!post && (
+
+      {!post && !loading && !error && (
         <button onClick={fetchRandomPost} disabled={loading}>
-          {loading ? 'Загрузка...' : 'Получить'}
+          Получить
         </button>
       )}
+
+      {loading && <Loader />}
       {error && <span>{error}</span>}
       {post && <span>{post.title}</span>}
     </>

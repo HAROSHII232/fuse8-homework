3263a06b40af0ef0ec2054616e4dad1111d15ca4
HAROSHII <igorgrime@gmail.com>
2025-03-17 17:13:53 +0300

    ref: Card component, add Props and styles

--- src/pages/landing/ui/card/card.tsx
+++ src/pages/landing/ui/card/card.tsx
@@ -1,15 +1,25 @@
+import { HTMLAttributes } from 'react';
 import styles from './card.module.scss';
+import cn from 'classnames';
 
 type Props = {
   title: string;
   description: string;
-};
+} & HTMLAttributes<HTMLDivElement>;
 
-export const Card = ({ title, description }: Props) => {
+export const Card = ({
+  title,
+  description,
+  children,
+  className,
+  ...rest
+}: Props) => {
+  const classNames = cn(styles.cardItem, className);
   return (
-    <div className={styles.cardItem}>
+    <div className={classNames} {...rest}>
       <h3>{title}</h3>
       <p>{description}</p>
+      {children}
     </div>
   );
 };
